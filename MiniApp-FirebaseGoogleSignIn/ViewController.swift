77cda053dab3bf274511ab095a18c0aa3a0e1fe1
mikekondo <foad5969@gmail.com>
2022-07-03 15:41:28 +0900

    firstcommit

---
+++ MiniApp-FirebaseGoogleSignIn/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  MiniApp-FirebaseGoogleSignIn
+//
+//  Created by 近藤米功 on 2022/07/03.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapSignInButton(_ sender: Any) {
+        auth()
+    }
+
+    private func login(credential: AuthCredential){
+        print("ログイン完了")
+    }
+
+    private func auth(){
+        // clientIDの取得
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // GIDの設定
+        let config = GIDConfiguration(clientID: clientID)
+
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self){ [unowned self] user, error in
+            if let error = error {
+                print("GIDSignInError: \(error.localizedDescription)")
+                return
+            }
+
+            guard let authentication = user?.authentication,let idToken = authentication.idToken else {
+                return
+            }
+
+            // 引数のuserの情報を使用して、GoogleAuthProviderのcredential(認証情報)を生成し、それを引数にFirebaseAuthのsignInを実行
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            self.login(credential: credential)
+        }
+    }
+
+
+
+}
+
